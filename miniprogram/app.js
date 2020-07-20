89b8373fa237c0fd3bb25d8813af0b340fc1f6cf
Dawn622 <dawn622@outlook.com>
2020-07-20 13:19:49 +0800

    homepage1.0

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -12,6 +12,7 @@ App({
         //   如不填则使用默认环境（第一个创建的环境）
         // env: 'my-env-id',
         traceUser: true,
+        env: "uqeasygo1"
       })
     }
 
---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,68 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    swiperPlaceholder:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/网上随便找的placeholder.jpg",
+    calcIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/Be- 倒计时.jpg",
+    countdownIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/Be - 计算器.jpg"
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
