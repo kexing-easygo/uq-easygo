6c68ccb5451f7af48b0bfc0ecd191d847a8c5991
Zian Wang <wza15046319911@github.com>
2020-08-15 23:54:56 +0800

    找回丢失的home.js文件

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,72 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    swiperPlaceholderOne:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/轮播图上线了.png",
+    swiperPlaceholderTwo:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/轮播图倒计时功能.png",
+    swiperPlaceholderThree:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/轮播图敬请期待.png",
+    calcIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/计算器.png",
+    countdownIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/倒计时.png",
+    inComingHolder:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/轮播图敬请期待.png",
+    inComingIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/敬请期待.png"
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
