aa045593e82ab8339d8cf04ba06e22fe5b99d768
Zian Wang <wza15046319911@github.com>
2021-02-13 19:52:34 +0800

    本次更新 1. 完成搜索框逻辑 2. 完成分数面板填写 3. 完成分数计算 还差历史信息加载 数据暂时是写死的

--- miniprogram/pages/calc/calc.js
+++ miniprogram/pages/calc/calc.js
@@ -9,91 +9,116 @@ Page({
    */
   data: {
     InComingImage:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/计算器敬请期待页面.png",
-<<<<<<< HEAD
     selectSemester: true,
     semester: "select semester",
+    // 测试数据
+    assessments: [
+      {name: "Online Problems", weight: 10},
+      {name: "Assignment 1", weight: 10},
+      {name: "Assignment 2", weight: 15},
+      {name: "Assignment 3", weight: 20},
+      {name: "Final exam", weight: 45},
+      // doublepass: true, 
+      // hurdle: 45, 
+    ],
+    result: "",
+    // 获得总分
+    totalScore: 0,
+    // 丢失总分
+    totalDropped: 0,
+    // 是否为doublePass
+    doublePass: true,
+    calculatedGPA: 0
 
   },
   
   /** 
-   * 获取键盘输入
+   * 获取键盘输入的作业实际得分
+   * 和总分数。批量化管理所有输入框
    */
   bindKeyInput: function (e) {
+    var key = e.currentTarget.id
+    var scoreType = e.currentTarget.dataset.model
+    var item = this.data.assessments[key]
+    // 根据输入框类型更新数值
+    if (scoreType == "score1") {
+      item["score1"] = e.detail.value
+    }
+    if (scoreType == "score2") {
+      item["score2"] = e.detail.value
+    }
+    // 如果两个输入框都有分数
+    // 且满足条件，开始计算分数
+    var percentage = item["score1"] / item["score2"]
+    if (percentage <= 1 && percentage >= 0) {
+      var mark = item["score1"] / item["score2"] * item.weight
+      // 更新已获得百分比和丢失百分比
+      item["score"] = mark
+      item["dropped"] = item.weight - mark
+    }
+    var totalScore = 0
+    var totalDropped = 0
+    for (var i = 0; i < this.data.assessments.length; i++) {
+      if (this.data.assessments[i]["score"] >= 0) {
+        totalScore += this.data.assessments[i]["score"]
+      }
+      totalDropped = 100 - totalScore
+    }
     this.setData({
-      inputValue: e.detail.value
+      totalScore: totalScore,
+      totalDropped: totalDropped,
+      calculatedGPA: this.calculateGPA(totalScore)
     })
-=======
-    inputVal: "",
-    isDoublePass: false,
-    matchedItems: []
->>>>>>> d952ba11b03b3f2f78f7b93ceef5c45cfd8a9d42
+  },
+  calculateGPA: function(score) {
+    if (score >= 50 && score < 64) {
+      return 4
+    } else if (score >= 64 && score < 75) {
+      return 5
+    } else if (score >= 75 && score < 84) {
+      return 6
+    } else if (score >= 85) {
+      return 7
+    } else {
+      return 3
+    }
   },
   /** 
    * 选择器功能
   */
 
 
-mySelect: function(e) {
-  this.setData({
-    selectSemester: false
-  })
-
-},
+  mySelect: function(e) {
+    this.setData({
+      selectSemester: false
+    })
+  },
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+  },
 
+  searchCourse: function() {
+    let value = this.data.result
+    let _this = this
+    // if (value.length == 8) {
+    //   // 数据库搜索
+    //   db.collection("Courses").where({
+    //     name: db.RegExp({
+    //       regexp: '^' + value
+    //     })
+    //   }).get({
+    //     success: function(res) {
+    //       console.log(res.data[0].assessment)
+    //       _this.setData({
+    //         assessments: res.data[0].assessment
+    //       })
+    //     }
+    //   })
+    // }
   },
-  showInput: function () {
-    this.setData({
-        inputShowed: true
-    });
-  },
-  hideInput: function () {
-    this.setData({
-        inputVal: "",
-        inputShowed: false
-    });
-  },
-  // 清空输入框内容
-  clearInput: function () {
-    this.setData({
-        inputVal: ""
-    });
-  },
-  // 获取输入框内容
-  inputTyping: function (e) {
-    var value = e.detail.value
-    // 获取本门课的信息
-    // 数据库用正则表达式进行模糊搜索    
-    db.collection("Courses").where({
-      name: db.RegExp({
-        regexp: '.*' + value,
-        options: "i"
-      })
-    }).get({
-      success: function(res) {
-          // res.data 是包含以上定义的两条记录的数组
-          // console.log("搜索到结果为")
-          // console.log(res.data)
-          for (var i = 0; i < data.length; i++) {
-            matchedItems.push(data[i].assessment)
-          }
-        }
-      })
-    // console.log(matchedItems)
-  },
-  extractCourseInfo: function(course) {
-    var courseName = course.name
-    var assessments = course.assessments
-    for (var i = 0; i < assessments.length; i++) {
-      var assessment = assessments[i]
-      var name = assessment.name
-      var weight = assessment.weight
-    }
 
-  },
   /**
    * Lifecycle function--Called when page is initially rendered
    */
