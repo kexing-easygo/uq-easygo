31e4344c38170e728a0572dce4e9cb0e369e4a9e
Zian Wang <wza15046319911@github.com>
2021-02-16 16:53:01 +0800

    【分次提交】 本次更新云函数接口 使之可以按照新版提醒（一天、三天、一周）来发送邮件

--- cloudfunctions/crontab/index.js
+++ cloudfunctions/crontab/index.js
@@ -2,7 +2,8 @@
 const cloud = require('wx-server-sdk')
 const cloudbase = require("@cloudbase/node-sdk");
 const app = cloudbase.init({
-    env: cloudbase.SYMBOL_CURRENT_ENV
+    // env: cloudbase.SYMBOL_CURRENT_ENV
+    env: "uqeasygo1"
 });
 const db = app.database();
 
@@ -10,10 +11,56 @@ cloud.init({
     env: cloud.DYNAMIC_CURRENT_ENV
 })
 
+async function send(email, name, type) {
+    var content = "您的" + name + "将于"
+    if (type == "oneDay") {
+        content += "一天之后due。"
+    } else if (type == "threeDay") {
+        content += "三天之后due。"
+    } else if (type == "oneWeek") {
+        content = content + "一周之后due。"
+    }
+    content += "请妥善处理您的有关事宜。\n\nUQ校园通团队"
+    var res = await cloud.callFunction({
+        // 要调用的云函数名称
+        name: 'sendEmail',
+        // 传递给云函数的参数
+        data: {
+            "toAddr": email,
+            "subject": "UQ校园通",
+            "content": content
+        }
+    })
+}
+
+async function update(openid, name, type) {
+    // 设置已经提醒过，提醒过不再二次提醒
+    var temp = ''
+    if (type == "oneDay") {
+        temp = "notification.oneDay"
+    } else if (type == "threeDay") {
+        temp = "notification.threeDay"
+    } else if (type == "oneWeek") {
+        temp = "notification.oneWeek"
+    }
+    const res = await db.collection('MainUser')
+    .where({
+        "_openid": openid,
+    })
+    .update({
+        // 将提醒设置为false
+        temp: false
+    })
+    console.log(res)
+}
+
 // date1 是now， date2是dueDate
-function dateDiff(date1, date2) {
-    // 转化为澳洲时间计算
-    var timeStamp = date1.getTime() + 2 * 60 * 60 * 1000
+function dateDiff(date1, date2, location) {
+    var timeStamp = date1.getTime()
+    if (location == "AU") {
+        // 转化为澳洲时间计算
+        timeStamp += 2 * 60 * 60 * 1000
+    } 
     var date1 = new Date(timeStamp)
     var new1 = new Date(date1.getFullYear().toString() + "/" + (date1.getMonth() + 1).toString() + "/" + date1.getDate().toString()).getTime()
     var new2 = new Date(date2.getFullYear().toString() + "/" + (date2.getMonth() + 1).toString() + "/" + date2.getDate().toString()).getTime()
@@ -39,103 +86,39 @@ async function getAllData() {
     const now = new Date()
     // 第一次循环，获取所有用户信息
     for (let i = 0; i < data.length; i++) {
-        const userAssignments = data[i].userAssignments
+        const userAssignments = data[i].userAssignment
         const email = data[i].userEmail
-        const emailNotification = data[i].emailNotification
         const openid = data[i]._openid
+        const notification = data[i].notification
+        const location = notification.location
+        const oneDay = notification.oneDay
+        const threeDay = notification.threeDay
+        const oneWeek = notification.oneWeek
+        const emailNotification = notification.emailNotification
         // 第二次循环，获取每个用户的所有作业
         for (let j = 0; j < userAssignments.length; j++) {
             const ass = userAssignments[j]
             const dueDate = new Date(ass.date)
             const name = ass.name
-            const notifications = ass.notification
             // 忽视due的时间，只计算日期
-            var dayDiff = dateDiff(now, dueDate)
+            var dayDiff = dateDiff(now, dueDate, location)
+            // console.log(dayDiff)
             // 检查是否相差正数天
             if (dayDiff >= 0 && emailNotification == true && email != "") {
-                if (dayDiff <= 1 && notifications["24"] == 1) {
-                    const content = "您的" + name + "将于" + "24小时之后due。"
-                    const res = await cloud.callFunction({
-                        // 要调用的云函数名称
-                        name: 'sendEmail',
-                        // 传递给云函数的参数
-                        data: {
-                            "toAddr": email,
-                            "subject": "UQ校园通",
-                            "content": content
-                        }
-                    }).then(res => {
-
-                    })
+                if (dayDiff <= 1 && oneDay == 1) {
+                    send(email, name, "oneDay")
                     // 设置已经提醒过，提醒过不再二次提醒
-                    db.collection('MainUser')
-                        .where({
-                            "_openid": openid,
-                            "userAssignments.name": name
-                        })
-                        .update({
-                            // 将提醒设置为false
-                            "userAssignments.$.notification.24": false
-                        })
-                        .then(res => {
-                            console.log(res.data)
-                        })
+                    update(openid, name, "oneDay")
                 }
-                if (dayDiff <= 2 && notifications["48"] == 1) {
-                    const content = "您的" + name + "将于" + "48小时之后due。"
-                    const res = await cloud.callFunction({
-                        // 要调用的云函数名称
-                        name: 'sendEmail',
-                        // 传递给云函数的参数
-                        data: {
-                            "toAddr": email,
-                            "subject": "UQ校园通",
-                            "content": content
-                        }
-                    }).then(res => {
-
-                    })
+                if (dayDiff <= 3 && threeDay == 1) {
+                    send(email, name, "threeDay")
                     // 设置已经提醒过，提醒过不再二次提醒
-                    db.collection('MainUser')
-                        .where({
-                            "_openid": openid,
-                            "userAssignments.name": name
-                        })
-                        .update({
-                            // 将提醒设置为false
-                            "userAssignments.$.notification.48": false
-                        })
-                        .then(res => {
-                            console.log(res.data)
-                        })
+                    update(openid, name, "threeDay")
                 }
-                if (dayDiff <= 3 && notifications["72"] == 1) {
-                    const content = "您的" + name + "将于" + "72小时之后due。"
-                    const res = await cloud.callFunction({
-                        // 要调用的云函数名称
-                        name: 'sendEmail',
-                        // 传递给云函数的参数
-                        data: {
-                            "toAddr": email,
-                            "subject": "UQ校园通",
-                            "content": content
-                        }
-                    }).then(res => {
-
-                    })
+                if (dayDiff <= 7 && oneWeek == 1) {
+                    send(email, name, "oneWeek")
                     // 设置已经提醒过，提醒过不再二次提醒
-                    db.collection('MainUser')
-                        .where({
-                            "_openid": openid,
-                            "userAssignments.name": name
-                        })
-                        .update({
-                            // 将提醒设置为false
-                            "userAssignments.$.notification.72": false
-                        })
-                        .then(res => {
-                            console.log(res.data)
-                        })
+                    update(openid, name, "oneWeek")
                 }
             }
         }
