bca7b66168a4b7ba3301bf8d6235fa3a9ae52f81
arthurqi <haitao.qi@uqconnect.edu.au>
2021-04-10 01:37:50 +1000

    本次修改： 1. 可从数据库拉数据，下一步，上传数据

--- miniprogram/pages/addTimeTable/addTimeTable.js
+++ miniprogram/pages/addTimeTable/addTimeTable.js
@@ -1,11 +1,27 @@
 // miniprogram/pages/addTimeTable/addTimeTable.js
+const db = wx.cloud.database()
+const app = getApp()
+const _ = db.command
+
+function get_class_start_end(start, duration) {
+  var hour = duration / 60;
+  var starts = start.split(":");
+  var end = parseInt(starts[0]) + hour + ":" + starts[1];
+  return start + " - " + end; 
+}
+
+function cl(content) {
+  console.log(content);
+}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    courseTitle:"csse1001",
+    courseTimeDeatial: {},
+    findTime: false,
   },
 
   /**
@@ -62,5 +78,63 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  timeCourse: function(e) {
+    this.setData({
+      courseTitle:e.detail.value.toUpperCase(),
+    });
+    console.log(this.data.course);
+  },
+  searchCourseTime: function() {
+   
+    var dic = {};
+    let that = this;
+    db.collection('Timetable').where({
+      course: that.data.courseTitle,
+    }).get({
+      success: function(res) {
+        
+        for (var key in res.data[0]) {
+          
+          if (key.length >= 20) {
+            var keyList = key.split("_");
+            
+            var mode = keyList[1] + keyList[3];
+            var i = 0;
+            
+            dic[mode] = [];
+            
+            
+            for (var keyItem in res.data[0][key]) {
+              var keyItemList = keyItem.split("|");
+              var temp = {};
+              temp["type"] = keyItemList[1];
+              var location = "-";
+              if (res.data[0][key][keyItem]["location"].length > 3) {
+                location = res.data[0][key][keyItem]["location"].split(" ")[0];
+              }
+              
+              temp["location"] = location;
+              temp["weekday"] = res.data[0][key][keyItem]["day_of_week"];
+              var startTime = res.data[0][key][keyItem]["start_time"];
+              var time = res.data[0][key][keyItem]["duration"];
+              
+              temp["start-end"] = get_class_start_end(startTime, time);
+              temp["start"] = startTime;
+              temp["hours"] = time / 60;
+              dic[mode].push(temp);    
+            }
+          }
+        }
+
+        that.setData({
+          courseTimeDeatial: dic,
+          findTime: true,
+        });
+        cl(that.data.courseTimeDeatial);
+      }
+    });
+
+    
   }
 })
--- miniprogram/pages/timetable/timetable.js
+++ miniprogram/pages/timetable/timetable.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/timetable/timetable.js
+
+var deatilTime;
 Page({
 
   /**
@@ -7,14 +9,40 @@ Page({
   data: {
     add: "cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/添加按钮.png",
     detailShow: false,
+    detailAnimation : "bottom: 0;animation: detailDownUp 1s;",
 
   },
+
+
 
   timeDetail: function(event){
     console.log(event.currentTarget.dataset['course']);
     this.setData({
       detailShow: true,
     });
+  },
+
+  timeDetailDown: function(event) {
+    this.setData({
+      //detailAnimation : "bottom: -545rpx;animation: detailUpDown 1s;",
+      detailShow: false,
+    });
+    // console.log(this.data['detailShow']);
+    // deatilTime = setTimeout(function() {
+    //   this.data.setData({
+    //     detailShow: false,
+    //   });
+    // } , 1000);
+  },
+
+  quitShade: function(event) {
+
+    this.setData({
+      // detailAnimation : "bottom: -545rpx;animation: detailUpDown 1s;",
+      detailShow: false,
+
+    });
+    clearTimeout(deatilTime);
   },
 
   /**
