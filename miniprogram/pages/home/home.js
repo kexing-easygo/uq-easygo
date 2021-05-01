692d6f2538fc0539dcb1ed0808854c76c72b28ec
Zian Wang <wza15046319911@github.com>
2021-05-01 16:24:58 +0800

    本次更新 1. 五月校历海报路径有问题，已修复 2. 修改主页图片加载路径

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -16,7 +16,7 @@ Page({
     timetableIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/课表图标.png",
     inComingIcon:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/敬请期待.png",
     // 下方非滚动海报
-    PostHolderOne:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/精简校历四月.png",
+    PostHolderOne:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/精简校历五月.png",
     PostHolderTwo:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/Bug征集.png",
     PostHolderThree:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/敬请期待海报.png"
   },
@@ -98,7 +98,7 @@ Page({
     return {
       title: 'UQ校园通',
       path: '/pages/home/home',
-      imageUrl: 'cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/精简校历三月.png'
+      imageUrl: this.data.PostHolderOne
     }
   },
   navigateToWaiting: function() {
