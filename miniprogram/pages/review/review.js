dadaca7bd9cd2d70ae241967198d38d0148cbc44
Kexu <douimp@qq.com>
2021-07-02 01:19:42 +1000

    课评功能UI初步开发

---
+++ miniprogram/pages/review/review.js
@@ -0,0 +1,73 @@
+// miniprogram/pages/review/review.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    courseName: "DECO1100 - Design Thinking",
+    courseLecturer: "Lecturer",
+    courseFaculty: "Faculty",
+    coursePre: ['DECO1100', 'DECO1100'],
+    courseSemester: ['Semester1', 'Semester2'],
+    like:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/课程表敬请期待.png",
+
+    reviewerInfo: "Sprite-2021 s1"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
