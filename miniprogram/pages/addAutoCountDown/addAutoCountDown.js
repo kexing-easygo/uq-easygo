f35deb20353e34e53c4ea28ff36a7f32f4e2687b
Zian Wang <wza15046319911@github.com>
2021-07-02 19:38:33 +0800

    本次修改 1. 自动添加倒计时功能灰度测试

--- miniprogram/pages/addAutoCountDown/addAutoCountDown.js
+++ miniprogram/pages/addAutoCountDown/addAutoCountDown.js
@@ -1,4 +1,16 @@
 // miniprogram/pages/addAutoCountDown.js
+const db = wx.cloud.database()
+const app = getApp()
+const _ = db.command
+function GetDateStr(AddDayCount) {
+  var dd = new Date();
+  dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期
+  var y = dd.getFullYear();
+  var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0
+  var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0
+  return y + "-" + m + "-" + d;
+}
+
 Page({
 
   /**
@@ -11,9 +23,39 @@ Page({
     clicked_3: false,
     clicked_4: false,
     clicked_5: false,
-
+    selectedAssessments: [],
+    oneDay: false,
+    threeDay: false,
+    oneWeek: false,
+    wechatNotification: false,
+    emailNotification: false,
+    templateID:'3xHIgiW1ROp8ig_32dTjPqVjNVsY-J4e6dekyW2Wn7U'
+  },
+  bindSwitch1: function (e) {
+    this.setData({
+      wechatNotification: e.detail.value
+    })
+    wx.getSetting({
+      withSubscriptions: true,
+      success (res) {
+        if (!res.subscriptionsSetting.itemSettings) {
+          wx.showModal({
+            title: '提示',
+            content: "系统检测到您未在订阅消息勾选总是保持以上选择，为保证您的体验，请一定要勾选哦！"
+          })
+        } else {
+          console.log(res.subscriptionsSetting.itemSettings)
+          
+        }
+      }
+    })
   },
 
+  bindSwitch2: function (e) {
+    this.setData({
+      emailNotification: e.detail.value
+    })
+  },
   bindRed: function() {
     this.setData({
       color: "#FA5151",
@@ -68,7 +110,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this;
+    // 读取全局缓存
+    console.log(app.globalData.selectedAssessments);
+    that.setData({
+      selectedAssessments: app.globalData.selectedAssessments
+    })
+    const eventChannel = that.getOpenerEventChannel()
+    eventChannel.on("acceptDataFromOpenerPage", function(d) {
+      that.setData({
+        selectedAssessments: d.data
+      })
+    })
   },
 
   /**
@@ -84,39 +137,188 @@ Page({
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  bindOneDay: function (e) {
+    let that = this
+    if (this.data.wechatNotification != true 
+      && this.data.emailNotification != true) {
+      wx.showModal({
+        title: "提示",
+        content: "你好像忘记设置微信或邮箱提醒了哦！"
+      })
+      return
+    }
+    if (this.data.wechatNotification == true && e.detail.value == true) {
+      var templateID = this.data.templateID
+      console.log("?")
+      wx.requestSubscribeMessage({
+        tmplIds: [templateID],
+        success (res) {
+          if (res['3xHIgiW1ROp8ig_32dTjPqVjNVsY-J4e6dekyW2Wn7U'] == 'accept') {
+            that.setData(
+              {
+                oneDay: true
+              }
+            )
+          } else {
+            that.setData(
+              {
+                oneDay: false
+              }
+            )
+          }
+        }
+      })
+    }
+    if (e.detail.value == false) {
+      that.setData(
+        {
+          oneDay: false
+        }
+      )
+    }
+    
   },
+  bindThreeDay: function (e) {
+    let that = this
+    if (this.data.wechatNotification != true 
+      && this.data.emailNotification != true) {
+      wx.showModal({
+        title: "提示",
+        content: "你好像忘记设置微信或邮箱提醒了哦！"
+      })
+      return
+    }
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    if (this.data.wechatNotification == true && e.detail.value == true) {
+      var templateID = this.data.templateID
+      wx.requestSubscribeMessage({
+        tmplIds: [templateID],
+        success (res) {
+          if (res['3xHIgiW1ROp8ig_32dTjPqVjNVsY-J4e6dekyW2Wn7U'] == 'accept') {
+            that.setData(
+              {
+                threeDay: true
+              }
+            )
+          } else {
+            that.setData(
+              {
+                threeDay: false
+              }
+            )
+          }
+        }
+      })
+    }
+    if (e.detail.value == false) {
+      that.setData(
+        {
+          threeDay: false
+        }
+      )
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  bindOneWeek: function (e) {
+    let that = this
+    if (this.data.wechatNotification != true 
+      && this.data.emailNotification != true) {
+      wx.showModal({
+        title: "提示",
+        content: "你好像忘记设置微信或邮箱提醒了哦！"
+      })
+      return
+    }
+    if (this.data.wechatNotification == true && e.detail.value == true) {
+      var templateID = this.data.templateID
+      wx.requestSubscribeMessage({
+        tmplIds: [templateID],
+        success (res) {
+          if (res['3xHIgiW1ROp8ig_32dTjPqVjNVsY-J4e6dekyW2Wn7U'] == 'accept') {
+            that.setData(
+              {
+                oneWeek: true
+              }
+            )
+          } else {
+            that.setData(
+              {
+                oneWeek: false
+              }
+            )
+          }
+        }
+      })
+    }
+    if (e.detail.value == false) {
+      that.setData(
+        {
+          oneWeek: false
+        }
+      )
+    }
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  requestSubscribe: function() {
+    var templateID = '3xHIgiW1ROp8ig_32dTjPqVjNVsY-J4e6dekyW2Wn7U'
+    wx.requestSubscribeMessage({
+      tmplIds: [templateID],
+      success (res) {
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  confirm: function() {
+    var color = this.data.color;
+    let that = this;
+    var ass = this.data.selectedAssessments;
+    for (var i = 0; i < ass.length; i++) {
+      // 更新color字段
+      ass[i].color = color;
+      // TODO: 解析到TBD，自动变为30天后
+      if (ass[i].date == "TBD") {
+        ass[i].date = GetDateStr(30);
+        ass[i].time = "00:00"
+      }
+      db.collection("MainUser")
+      .where({
+        _openid: app.globalData._openid
+      })
+      .update({
+        data: {
+          userAssignments: _.push(ass[i])
+        },
+        success: function (res) {
+          if (res.stats.updated > 0) {
+            console.log("作业条目添加成功")
+          }
+        }
+      })
+    }
+    db.collection("MainUser")
+    .where({
+      _openid: app.globalData._openid
+    })
+    .update({
+      data: {
+        notification: {
+          wechatNotification: that.data.wechatNotification,
+          emailNotification: that.data.emailNotification,
+          oneDay: that.data.oneDay,
+          threeDay: that.data.threeDay,
+          oneWeek: that.data.oneWeek
+        }
+      },
+      success: function (res) {
+        console.log(res)
+      }
+    })
+    wx.reLaunch({
+      url: '/pages/countdown/countdown',
+      success: function (res) {
+        var page = getCurrentPages().pop()
+        if (page == undefined || page == null) return;
+        // 刷新页面
+        page.onLoad()
+      }
+    })
   }
 })
--- miniprogram/pages/autoCountDown/autoCountDown.js
+++ miniprogram/pages/autoCountDown/autoCountDown.js
@@ -13,8 +13,9 @@ Page({
     showResult: false,
     assessments: [],
     warning: true,
-    searchBarValue: ""
-    // search: ""
+    searchBarValue: "",
+    selectedAssessments: []
+    
   },
   bindCourseInput: function (e) {
     this.setData({
@@ -31,7 +32,28 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    // this.fetchCourseInfo();
+    this.fetchCourseInfo();
+  },
+  /**
+   * 绑定checkbox-group的回调函数
+   * 
+   * @param {e} 事件；
+   * e.detail.value 代表被选中的checkbox的index数组
+   * 也代表某个作业在assessments列表中的index
+   */
+  selectAssessments: function (e) {
+    var selectedValues = e.detail.value;
+    // 被选中的作业
+    var temp = [];
+    for (var i = 0; i < selectedValues.length; i++) {
+      // index对应选中的作业在assessments里面的的index
+      var index = parseInt(selectedValues[i]);
+      temp.push(this.data.assessments[index]);
+    }
+    // console.log(temp);
+    this.setData({
+      selectedAssessments: temp
+    })
   },
 
   /**
@@ -53,12 +75,12 @@ Page({
    * 返回数据库中该门课的信息
    * course: 用户输入的course name
    * return: 该门课external的作业信息(array)
-   * 这他妈异步请求我丢
+   * date字段被分成date和time以便于接下来添加
    * 
    */
   fetchCourseInfo: function() {
-    var course = this.data.searchBarValue;
-    // var course = "CSSE1001"
+    // var course = this.data.searchBarValue;
+    var course = "CSSE1001"
     let that = this;
     if (course.length == 8) {
       db.collection('CourseReview')
@@ -80,17 +102,42 @@ Page({
             // TODO: 根据用户上课模式返回结果
             // 默认返回external
             var assessments = res.data[0]["external"]["assessments"];
+            // 解析date字段，分成date和time
+            for (var i = 0; i < assessments.length; i++) {
+              var ass = assessments[i];
+              var date_string = ass["date"];
+              var date = date_string.split(" ")[0];
+              var time = date_string.split(" ")[1];
+              assessments[i]["date"] = date;
+              assessments[i]["time"] = time;
+            }
             // showResult要设置为true才可以显示搜索结果
             that.setData({
               showResult: true,
               assessments: assessments
             })
-            console.log(this.data.assessments);
           }
         },
       )
     }
     
+  },
+  /**
+   * 下一步按钮的回调函数，将选中的作业列表
+   * 传递到下一个页面
+   */
+  nextStep: function () {
+    let that = this;
+    // 将用户选中的作业缓存至全局
+    // TODO 我也不知道为啥一样的代码在这就不好使 MD
+    app.globalData.selectedAssessments = that.data.selectedAssessments;
+    wx.navigateTo({
+      url: '../addAutoCountDown/addAutoCountDown',
+      success: function (res) {
+        // 通过eventChannel向被打开页面传送正在被点击的assignment信息
+        res.eventChannel.emit('acceptDataFromOpenerPage', {data: that.data.selectAssessments});
+      }
+    })
   }
 
 })
