1833f83ba461c4903555934ccca3d0d05d3e2227
Zian Wang <wza15046319911@github.com>
2021-07-05 21:09:01 +0800

    本次修改 1. 用户可以设置自己的上课模式啦！

--- miniprogram/pages/basicUserInfo/basicUserInfo.js
+++ miniprogram/pages/basicUserInfo/basicUserInfo.js
@@ -1,6 +1,6 @@
 // pages/basicUserInfo/basicUserInfo.js
 const app = getApp()
-
+const db = wx.cloud.database()
 Page({
 
   /**
@@ -19,6 +19,26 @@ Page({
       index: e.detail.value,
       classMode: this.data.array[e.detail.value],
     })
+    let that = this;
+    db.collection('MainUser')
+    .where({
+      // _openid: app.globalData._openid
+      _openid: "oe4Eh5T-KoCMkEFWFa4X5fthaUG8"
+
+    })
+    .update({
+      data: {
+        // 表示将 done 字段置为 true
+        classMode: that.data.classMode
+      },
+      success: function(res) {
+        wx.showToast({
+          title: '更新成功',
+          icon: 'none',
+          duration: 1000
+        })
+      }
+    })
   },
 
   setMode: function (e) {
@@ -33,6 +53,23 @@ Page({
   onLoad: function (options) {
     let that = this
     if (app.globalData.hasUserInfo) {
+      // 获取用户上课模式
+      var classMode = "点击设置"
+      db.collection('MainUser')
+      .where({
+        // _openid: app.globalData._openid
+        _openid: "oe4Eh5T-KoCMkEFWFa4X5fthaUG8"
+
+      })
+      .get()
+      .then(res => {
+        if ('classMode' in res.data[0]) {
+          classMode = res.data[0].classMode;
+        }
+        that.setData({
+          classMode: classMode
+        })
+      })
       that.setData({
         hasUserInfo: true,
         nickName: app.globalData.userInfo.nickName
