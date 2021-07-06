3a2154b3baca3d8f35d3f0b05cb5b16d9719a05f
Zian Wang <wza15046319911@github.com>
2021-07-06 22:58:06 +0800

    本次修改 1. 搜索结果的回调函数绑定 2. 搜索结果传递到第二个页面

--- miniprogram/pages/review/review.js
+++ miniprogram/pages/review/review.js
@@ -10,17 +10,50 @@ Page({
     courseFaculty: "Faculty",
     coursePre: ['DECO1100', 'DECO1100'],
     courseSemester: ['Semester1', 'Semester2'],
+    courseIncompatible: "DECO1100",
     like:"cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/课程表敬请期待.png",
-
+    easy_hd: 0,
+    easy_pass: 0,
+    good_luck: 0,
+    hard_pass: 0,
     reviewerInfo: "Sprite-2021 s1",
     ownReview: true,
+    reviews: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this
+    const eventChannel = that.getOpenerEventChannel()
+    eventChannel.on('acceptDataFromOpenerPage', function (e) {
+      var raw = JSON.parse(e)
+      var data = raw.data;
+      console.log(data);
+      // 赋值
+      var prerequisite = "None"
+      if ("prerequisite" in data.external) {
+        prerequisite = data.external.prerequisite;
+      }
+      var incompatible = "None"
+      if ("incompatible" in data.external) {
+        incompatible = data.external.incompatible;
+      }
+      that.setData({
+        courseName: data.course_name,
+        courseLecturer: data.external.lecturer,
+        courseFaculty: data.external.faculty,
+        coursePre: prerequisite,
+        courseIncompatible: incompatible,
+        courseSemester: data.external.taught,
+        easy_hd: data.easy_hd,
+        easy_pass: data.easy_pass,
+        good_luck: data.good_luck,
+        hard_pass: data.hard_pass,
+        reviews: data.reviews
+      })
+    })
   },
 
   /**
--- miniprogram/pages/searchReview/searchReview.js
+++ miniprogram/pages/searchReview/searchReview.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/searchReview/searchReview.js
+const app = getApp()
+const db = wx.cloud.database()
 Page({
 
   /**
@@ -7,9 +9,46 @@ Page({
   data: {
     searchError: false,
     buttons: [{text: '取消'}, {text: '确认'}],
-
+    searchBarValue: ''
   },
-
+  bindCourseInput: function (e) {
+    this.setData({
+      searchBarValue: e.detail.value
+    })
+  },
+  fetchCourseInfo: function () {
+    // 检查用户是否选择上课模式
+    var title = this.data.searchBarValue;
+    db.collection("CourseReview")
+    .where({
+      course_name: title
+    })
+    .get()
+    .then(
+      res => {
+        if (res.data.length == 0) {
+          wx.showModal({
+            title: 'UU妹提醒',
+            content: '这门课超出了U妹的搜索范围。请确认好课号重新输入或微信联系U妹～',
+            success(res) {}
+          })
+          return;
+        } else {
+          var temp = res.data[0];
+          var query = JSON.stringify({
+            data: temp
+          })
+          wx.navigateTo({
+            url: '/pages/review/review',
+            success: function (res) {
+              // 通过eventChannel向被打开页面传送正在被点击的assignment信息
+              res.eventChannel.emit('acceptDataFromOpenerPage', query)
+            }
+          })
+        }
+      }
+    )
+  },
   /**
    * 生命周期函数--监听页面加载
    */
