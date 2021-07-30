5b16680717058aeab3474db9c557f5c2669712bb
Zian Wang <wza15046319911@github.com>
2021-07-31 00:41:54 +0800

    更改一个小bug，准备将课评合并到分支

--- miniprogram/pages/autoCountDown/autoCountDown.js
+++ miniprogram/pages/autoCountDown/autoCountDown.js
@@ -2,6 +2,15 @@
 const db = wx.cloud.database()
 const app = getApp()
 const _ = db.command
+function GetDateStr(AddDayCount) {
+  var dd = new Date();
+  dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期
+  var y = dd.getFullYear();
+  var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0
+  var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0
+  return y + "-" + m + "-" + d;
+}
+
 Page({
 
   /**
@@ -138,10 +147,10 @@ Page({
    * 
    */
   fetchCourseInfo: function() {
-    var course = this.data.searchBarValue;
+    var course = this.data.searchBarValue.toUpperCase();
     let that = this;
     if (course.length == 8) {
-      db.collection('CourseReview')
+      db.collection('CourseNew')
       .where({
         course_name: course
       })
