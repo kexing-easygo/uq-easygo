fc6c56f92bba4770e286f96d897a979efdcf04bc
arthurqi <haitao.qi@uqconnect.edu.au>
2021-08-05 16:17:49 +1000

    本次修改： 1. 自动添加bug修复

--- miniprogram/pages/autoCountDown/autoCountDown.js
+++ miniprogram/pages/autoCountDown/autoCountDown.js
@@ -2,6 +2,7 @@
 const db = wx.cloud.database()
 const app = getApp()
 const _ = db.command
+
 function GetDateStr(AddDayCount) {
   var dd = new Date();
   dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期
@@ -32,13 +33,17 @@ Page({
     beforeBlue: "cloud://uqeasygo1.7571-uqeasygo1-1302668990/image/颜色选择器/未蓝色选择器.png",
 
     setClassMode: true,
-    buttons: [{text: '好的'}, {text: '这就去设置'}],
+    buttons: [{
+      text: '好的'
+    }, {
+      text: '这就去设置'
+    }],
     showResult: false,
     assessments: [],
     warning: true,
     searchBarValue: "",
     selectedAssessments: []
-    
+
   },
   bindCourseInput: function (e) {
     this.setData({
@@ -46,7 +51,7 @@ Page({
     })
   },
 
-  bindRed: function() {
+  bindRed: function () {
     this.setData({
       color: "#FF7043",
       clicked_1: true,
@@ -57,7 +62,7 @@ Page({
     });
   },
 
-  bindYellow: function() {
+  bindYellow: function () {
     this.setData({
       color: "#FFB300",
       clicked_1: false,
@@ -68,7 +73,7 @@ Page({
     });
   },
 
-  bindGreen: function() {
+  bindGreen: function () {
     this.setData({
       color: "#8BC34A",
       clicked_1: false,
@@ -79,7 +84,7 @@ Page({
     });
   },
 
-  bindBlue: function() {
+  bindBlue: function () {
     this.setData({
       color: "#29B6F6",
       clicked_1: false,
@@ -90,7 +95,7 @@ Page({
     });
   },
 
-  bindGrey: function() {
+  bindGrey: function () {
     this.setData({
       color: "#576B95",
       clicked_1: false,
@@ -100,12 +105,11 @@ Page({
       clicked_5: true,
     });
   },
-  
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -139,7 +143,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
 
@@ -157,46 +161,46 @@ Page({
    * date字段被分成date和time以便于接下来添加
    * 
    */
-  fetchCourseInfo: function() {
+  fetchCourseInfo: function () {
     var course = this.data.searchBarValue.toUpperCase();
     let that = this;
     if (course.length == 8) {
       db.collection('CourseNew')
-      .where({
-        course_name: course
-      })
-      .get()
-      .then(
-        res =>{
-          if (res.data.length == 0) {
-            wx.showModal({
-              title: '温馨提示',
-              content: "这门课太难了，超出了U妹目前的搜索范围。请确定课号重新输入或者微信联系U妹～",
-              success(res) {
-                return
+        .where({
+          course_name: course
+        })
+        .get()
+        .then(
+          res => {
+            if (res.data.length == 0) {
+              wx.showModal({
+                title: '温馨提示',
+                content: "这门课太难了，超出了U妹目前的搜索范围。请确定课号重新输入或者微信联系U妹～",
+                success(res) {
+                  return
+                }
+              })
+            } else {
+              // TODO: 根据用户上课模式返回结果
+              // 默认返回external
+              var assessments = res.data[0]["external"]["assessments"];
+              // 解析date字段，分成date和time
+              for (var i = 0; i < assessments.length; i++) {
+                var ass = assessments[i];
+                var date_string = ass["date"];
+                var date = date_string.split(" ")[0];
+                var time = date_string.split(" ")[1];
+                assessments[i]["date"] = date;
+                assessments[i]["time"] = time;
               }
-            })
-          } else {
-            // TODO: 根据用户上课模式返回结果
-            // 默认返回external
-            var assessments = res.data[0]["external"]["assessments"];
-            // 解析date字段，分成date和time
-            for (var i = 0; i < assessments.length; i++) {
-              var ass = assessments[i];
-              var date_string = ass["date"];
-              var date = date_string.split(" ")[0];
-              var time = date_string.split(" ")[1];
-              assessments[i]["date"] = date;
-              assessments[i]["time"] = time;
+              // showResult要设置为true才可以显示搜索结果
+              that.setData({
+                showResult: true,
+                assessments: assessments
+              })
             }
-            // showResult要设置为true才可以显示搜索结果
-            that.setData({
-              showResult: true,
-              assessments: assessments
-            })
-          }
-        },
-      )
+          },
+        )
     }
   },
   /**
@@ -215,7 +219,6 @@ Page({
       })
       return
     }
-    
     for (var i = 0; i < ass.length; i++) {
       // 更新color字段
       ass[i].color = color;
@@ -225,33 +228,49 @@ Page({
       //   ass[i].date = GetDateStr(30);
       //   ass[i].time = "00:00"
       // }
-      db.collection("MainUser")
+    }
+    //获取数据库的原有内容，本地修改，然后直接替换
+    db.collection('MainUser')
       .where({
         _openid: app.globalData._openid
       })
-      .update({
-        data: {
-          userAssignments: _.push(ass[i])
-        },
+      .get({
         success: function (res) {
-          if (res.stats.updated > 0) {
-            console.log("作业条目添加成功")
-            // reLaunch方法一定要在异步方法内部调用，保证先后顺序
-            // 不然会出现更新延迟的问题
-            wx.redirectTo({
-              url: '../countdown/countdown',
+          var temp = res.data[0].userAssignments;
+          console.log(temp);
+          for (var i = 0; i < temp.length; i++) {
+            ass.push(temp[i]);
+          }
+
+          db.collection("MainUser")
+            .where({
+              _openid: app.globalData._openid
+            })
+            .update({
+              data: {
+                userAssignments: ass
+              },
               success: function (res) {
-                var page = getCurrentPages().pop()
-                if (page == undefined || page == null) return;
-                // 刷新页面
-                page.onLoad()
+                if (res.stats.updated > 0) {
+                  console.log("作业条目添加成功")
+                  // reLaunch方法一定要在异步方法内部调用，保证先后顺序
+                  // 不然会出现更新延迟的问题
+                  wx.redirectTo({
+                    url: '../countdown/countdown',
+                    success: function (res) {
+                      var page = getCurrentPages().pop()
+                      if (page == undefined || page == null) return;
+                      // 刷新页面
+                      page.onLoad()
+                    }
+                  })
+                }
               }
             })
-          }
+
         }
       })
-    }
-    
+
   }
 
 })
