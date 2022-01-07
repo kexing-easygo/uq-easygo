25d0d21d2e55027c9c6d5c27f8c3c3cd95fb4fc6
Zian Wang <wza15046319911@github.com>
2022-01-07 13:01:50 +0800

    [fix] 删除多余的文件

--- client/src/utils/utils/assessments.js
+++
@@ -1,9 +0,0 @@
-export const mergeAssResult = (assessments, results) => {
-  return assessments.map(ass => {
-    const calculated = results.find(res => res.description === ass.description);
-    return {
-      ...ass,
-      ...calculated
-    }
-  })
-}
--- client/src/utils/utils/profile.js
+++
@@ -1,3 +0,0 @@
-import { ICON_SIZE, ICON_COLOR } from './constant'
-export const getIcon = iconName =>
-  ({ size: ICON_SIZE, color: ICON_COLOR, value: iconName });
