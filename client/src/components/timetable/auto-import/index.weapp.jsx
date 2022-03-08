cceccf2e9249d753ad34ee698f1e8166b1afab46
Zian Wang <wza15046319911@github.com>
2022-03-08 16:25:58 +1100

    [feat] 一键导入前端界面；

--- client/src/components/timetable/auto-import/index.weapp.jsx
+++ client/src/components/timetable/auto-import/index.weapp.jsx
@@ -6,7 +6,8 @@ import { AtFloatLayout, AtList, AtListItem, AtButton, AtInput } from "taro-ui";
 import "./index.less";
 import { setShowAutoImport } from "../../../features/course-slice";
 import { CURRENT_SEMESTER } from "../../../utils/constant";
-import { autoImportTimetable } from "../../../services/course";
+import { autoImportTimetable, fetchSelectedCourses } from "../../../services/course";
+import { TIMETABLE_SUBSCRIBE_LINK_REGEX } from "../../../utils/constant";
 
 import {
   AtModal,
@@ -23,11 +24,24 @@ export default function AutoImport(props) {
   const { currentClasses } = useSelector((state) => state.course);
   const dispatch = useDispatch();
 
+  const handleClipboardData = () => {
+    Taro.getClipboardData({
+      success: function(res) {
+        if (res.data != "") {
+          if (res.data.match(TIMETABLE_SUBSCRIBE_LINK_REGEX) != null) {
+            setLink(res.data);
+          }
+        }
+      },
+    });
+  }
+
   /**
    * 用户点击“输入课表链接”时的回调函数。
    * 如果用户有添加过课程，则会提示覆盖，如果没添加过，什么都不提示
    */
   const handleImport = () => {
+    // 如果用户有重新选择过课程
     if (currentClasses.length > 0) {
       Taro.showModal({
         title: "温馨提示",
@@ -37,26 +51,18 @@ export default function AutoImport(props) {
         cancelText: "取消",
         success: (res) => {
           if (res.confirm) {
+            // 读取剪贴板内容
+            // 如果格式符合要求，自动为用户粘贴
             setLinkInput(true);
-            Taro.getClipboardData({
-              success: function(res) {
-                if (res.data != "") {
-                  setLink(res.data);
-                }
-              },
-            });
+            handleClipboardData()
           }
         },
       });
     } else {
+      // 读取剪贴板内容
+            // 如果格式符合要求，自动为用户粘贴
       setLinkInput(true);
-      Taro.getClipboardData({
-        success: function(res) {
-          if (res.data != "") {
-            setLink(res.data);
-          }
-        },
-      });
+      handleClipboardData()
     }
   };
 
@@ -176,11 +182,27 @@ export default function AutoImport(props) {
             type="primary"
             size="small"
             onClick={() => {
+              if (link.match(TIMETABLE_SUBSCRIBE_LINK_REGEX) == null) {
+                Taro.showModal({
+                  title: "温馨提示",
+                  content: "您的导入链接看起来有点问题呀～是否查看教程？",
+                  success: (res) => {
+                    if (res.confirm) {
+                      // 跳转公众号
+                    }
+                  },
+                  confirmText: "查看教程",
+                  cancelText: "再试一次"
+                })
+                return
+              }
               dispatch(
                 autoImportTimetable({
                   timetableLink: link,
                 })
               );
+              // 刷新课程
+              dispatch(fetchSelectedCourses())
               setLinkInput(false);
             }}
           >
