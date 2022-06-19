d4e97ac8c02a083177ff807d4b5bb3fcb45dc6a6
Zian Wang <wza15046319911@github.com>
2022-06-19 12:10:12 +1000

    [feat] 1. 主页活动上新 2. 有主评被添加时，新增被提醒人名单

--- cloud/functions/review/index.js
+++ cloud/functions/review/index.js
@@ -61,26 +61,60 @@ async function addReview(collectionName, reviewObj) {
         }
     })
     omitSubReview(reviewobject)
-    // const {
-    //     posterName,
-    //     courseCode,
-    //     openid
-    // } = reviewObj
-    // const content = `
-    // ${posterName}刚刚在${courseCode}发表了一条主评，内容为：${reviewContent}。\n\n
-    // 为了方便追溯该用户信息，请参考以下数据：\n\n
-    // openid: ${openid}\n
-    // review id：${reviewobject.review_id}`
-    // await cloud.callFunction({
-    //     // 要调用的云函数名称
-    //     name: 'email',
-    //     // 传递给云函数的参数
-    //     data: {
-    //         toAddr: "913248383@qq.com",
-    //         content: "告警测试\n\n\n" + content,
-    //         subject: "课行校园通 - 课评addReview告警"
-    //     }
-    // })
+    const {
+        posterName,
+        courseCode,
+        openid
+    } = reviewObj
+    const content = `
+    ${posterName}刚刚在${courseCode}发表了一条主评，内容为：${reviewContent}。\n\n
+    为了方便追溯该用户信息，请参考以下数据：\n\n
+    openid: ${openid}\n
+    review id：${reviewobject.review_id}`
+    const mailList = ["913248383@qq.com" ,"251429669@qq.com","lyxhahcjy@outlook.com","15265252903@163.com"]
+    await cloud.callFunction({
+        // 要调用的云函数名称
+        name: 'email',
+        // 传递给云函数的参数
+        data: {
+            toAddr: "913248383@qq.com",
+            content: "主评添加提醒\n\n\n" + content,
+            subject: "课行校园通 - 主评添加提醒"
+        }
+    })
+    
+    await cloud.callFunction({
+        // 要调用的云函数名称
+        name: 'email',
+        // 传递给云函数的参数
+        data: {
+            toAddr: "251429669@qq.com",
+            content: "主评添加提醒\n\n\n" + content,
+            subject: "课行校园通 - 主评添加提醒"
+        }
+    })
+
+    await cloud.callFunction({
+        // 要调用的云函数名称
+        name: 'email',
+        // 传递给云函数的参数
+        data: {
+            toAddr: "lyxhahcjy@outlook.com",
+            content: "主评添加提醒\n\n\n" + content,
+            subject: "课行校园通 - 主评添加提醒"
+        }
+    })
+
+    await cloud.callFunction({
+        // 要调用的云函数名称
+        name: 'email',
+        // 传递给云函数的参数
+        data: {
+            toAddr: "15265252903@163.com",
+            content: "主评添加提醒\n\n\n" + content,
+            subject: "课行校园通 - 主评添加提醒"
+        }
+    })
 
     return reviewobject
 }
