14d8a560779cfd5041200c922461eda8aae64149
Zian Wang <wza15046319911@github.com>
2022-07-19 22:46:42 +1000

    [fix]还原之前丢失的sub-review检查的文件

--- client/src/features/review-slice.js
+++ client/src/features/review-slice.js
@@ -1,56 +1,78 @@
-import Taro from '@tarojs/taro'
-import { createSlice } from '@reduxjs/toolkit'
-import { fetchHotResearches,  fetchCourseInfo, addReviewDimensions } from '../services/review'
-import { fetchReviews, deleteReview, addReview, updateReview, searchCourses } from '../services/review'
-import { fetchSubReviews, addSubReview ,updateSubReview, deleteSubReview } from '../services/review'
-import { fetchUncheckedReviews, markReviewAsFailed, markReviewAsPassed } from "../services/checkReviews";
-import { updateLikes } from '../services/review'
+import Taro from "@tarojs/taro";
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  fetchHotResearches,
+  fetchCourseInfo,
+  addReviewDimensions
+} from "../services/review";
+import {
+  fetchReviews,
+  deleteReview,
+  addReview,
+  updateReview,
+  searchCourses
+} from "../services/review";
+import {
+  fetchSubReviews,
+  addSubReview,
+  updateSubReview,
+  deleteSubReview
+} from "../services/review";
+import {
+  fetchUncheckedReviews,
+  markReviewAsFailed,
+  markReviewAsPassed,
+  markSubReviewAsPassed,
+  markSubReviewAsFailed,
+  fetchAllSubReviews
+} from "../services/checkReviews";
+import { updateLikes } from "../services/review";
 import { REVIEW_CHECKED_TEMPLATE_ID } from "../config.json";
 
 const initialState = {
   hotCourses: [], // 热搜课程编号
-  searchedCourse: '', // 搜索的课程编号
+  searchedCourse: "", // 搜索的课程编号
   courseInfo: {}, // 课程信息
   dimensions: [], //  好过, 好难， 好7， 运气 各自总数
   reviewedIcon: 4, // 评论过的小图标
-  clickedIconReview: '', // 点击的小图标
+  clickedIconReview: "", // 点击的小图标
   reviews: [], // 课评
   clickedReview: {}, // 点击的课评
   editModal: false, // 课评修改模式
   likesReviews: [], // 赞过的课评 元素是review Id
-  subReviews:[], // 追评
+  subReviews: [], // 追评
   editSubReview: false, // 追评修改模式
   clickedSubReview: {}, // 点击的追评
   turnPage: [], // 判断 是否全都获取到 课程信息, 课评,
-  uncheckedReviews: [] ,
+  uncheckedReviews: [],
   showContent: false,
   floatTitle: false,
   modalTitle: false,
-  allSubReviews:[],
+  allSubReviews: [],
   relevantCourse: [], // 搜索到的 相关的 课程代码
-  searchedCourse: null, // 有无 相关课程
-}
+  searchedCourse: null // 有无 相关课程
+};
 
 export const reviewSlice = createSlice({
-  name: 'review',
+  name: "review",
   initialState,
   reducers: {
     // 设置搜索的课程代码
     setSearchedCourse: (state, action) => {
-      state.searchedCourse = action.payload
+      state.searchedCourse = action.payload;
     },
     // 设置点击的课评
     setClickedReview: (state, action) => {
-      state.clickedReview = action.payload
+      state.clickedReview = action.payload;
     },
     // 未评论时 点击的小图标
     setClickedIconReview: (state, action) => {
-      state.clickedIconReview = action.payload
+      state.clickedIconReview = action.payload;
     },
-     // 设置课评修改模式
-     setEditModal: (state, action) => {
+    // 设置课评修改模式
+    setEditModal: (state, action) => {
       state.editModal = action.payload;
-    }, 
+    },
     // 设置追评修改模式
     // 设置追评修改模式
     setSubReviewEdit: (state, action) => {
@@ -60,7 +82,7 @@ export const reviewSlice = createSlice({
     setClickedSubReview: (state, action) => {
       state.clickedSubReview = action.payload;
     },
-    // 清空 页面跳转条件 -- 获取课程信息, 课评 
+    // 清空 页面跳转条件 -- 获取课程信息, 课评
     setTurnPage: (state, action) => {
       state.turnPage = [];
     },
@@ -69,31 +91,35 @@ export const reviewSlice = createSlice({
       state.replySubReview = action.payload;
     },
     showContentState: (state, action) => {
-      state.showContent = action.payload
+      state.showContent = action.payload;
     },
     setFloatTitle: (state, action) => {
-      state.floatTitle = action.payload
+      state.floatTitle = action.payload;
     },
     setModalTitle: (state, action) => {
-      state.modalTitle = action.payload
+      state.modalTitle = action.payload;
     },
     // 清除搜索的相关课程
     setRelevantCourse: (state, action) => {
-      state.relevantCourse = action.payload
+      state.relevantCourse = action.payload;
     }
   },
-  extraReducers: (builder) => {
+  extraReducers: builder => {
     builder
       // 获取热搜课程
       .addCase(fetchHotResearches.pending, () => {
-        Taro.showToast({ title: '加载热搜课程中', icon: 'loading', mask: true})
+        Taro.showToast({
+          title: "加载热搜课程中",
+          icon: "loading",
+          mask: true
+        });
       })
       .addCase(fetchHotResearches.fulfilled, (state, action) => {
-        state.hotCourses = action.payload
+        state.hotCourses = action.payload;
       })
       // 获取课评
       .addCase(fetchReviews.pending, () => {
-        Taro.showToast({ title: '搜索中', icon: 'loading' })
+        Taro.showToast({ title: "搜索中", icon: "loading" });
       })
       .addCase(fetchReviews.fulfilled, (state, action) => {
         state.reviews = action.payload[0];
@@ -103,75 +129,75 @@ export const reviewSlice = createSlice({
         state.courseInfo = action.payload[4];
         Taro.navigateTo({
           url: "/pages/review-result/index"
-        })
+        });
       })
       .addCase(fetchReviews.rejected, (state, action) => {
-        Taro.showToast({ title: '暂时没有这门课哦', icon: 'none' })
+        Taro.showToast({ title: "暂时没有这门课哦", icon: "none" });
       })
       // 添加课评
       .addCase(addReview.fulfilled, (state, action) => {
-        state.reviews.unshift(action.payload)
+        state.reviews.unshift(action.payload);
         Taro.showModal({
           title: "发表通知",
           content: "您的评论已经发表，正在交由后台审核。",
           complete: () => {
             Taro.requestSubscribeMessage({
               tmplIds: [REVIEW_CHECKED_TEMPLATE_ID],
-              success: (res) => {
-                
-              }
-            })
+              success: res => {}
+            });
           }
-        })
-        
+        });
       })
       .addCase(addReview.rejected, () => {
-        Taro.showToast({ title: '添加课评时出错了', icon: 'none' })
+        Taro.showToast({ title: "添加课评时出错了", icon: "none" });
       })
       // 删除课评
       .addCase(deleteReview.fulfilled, (state, action) => {
-        for (let i=0; i<state.reviews.length; i++) {
+        for (let i = 0; i < state.reviews.length; i++) {
           if (state.reviews[i].review_id == action.payload) {
             state.reviews.splice(i, 1);
-        }}
-        Taro.showToast({ title: '删除成功', icon: 'none' })
+          }
+        }
+        Taro.showToast({ title: "删除成功", icon: "none" });
       })
       .addCase(deleteReview.rejected, () => {
-        Taro.showToast({ title: '删除课评时出错了', icon: 'none' })
+        Taro.showToast({ title: "删除课评时出错了", icon: "none" });
       })
       // 修改课评
       .addCase(updateReview.fulfilled, (state, action) => {
-        for (let i=0; i<state.reviews.length; i++) {
+        for (let i = 0; i < state.reviews.length; i++) {
           if (state.reviews[i].review_id == action.payload[0]) {
             state.reviews.splice(i, 1);
-        }}
-        Taro.showToast({ title: '修改成功', icon: 'success' });
+          }
+        }
+        Taro.showToast({ title: "修改成功", icon: "success" });
         state.reviews.unshift(action.payload[1]);
       })
       .addCase(updateReview.rejected, () => {
-        Taro.showToast({ title: '修改课评时出错了', icon: 'none' })
+        Taro.showToast({ title: "修改课评时出错了", icon: "none" });
       })
       // 添加小图标评论
       .addCase(addReviewDimensions.fulfilled, (state, action) => {
-        state.reviewedIcon = action.payload
-        state.dimensions[action.payload] ++;
+        state.reviewedIcon = action.payload;
+        state.dimensions[action.payload]++;
       })
       .addCase(addReviewDimensions.rejected, () => {
-        Taro.showToast({ title: '添加评论时出错了', icon: 'none' })
+        Taro.showToast({ title: "添加评论时出错了", icon: "none" });
       })
 
-      // 点赞 
+      // 点赞
       .addCase(updateLikes.fulfilled, (state, action) => {
         // 更新课评界面 小图标样式
-        state.likesReviews.push(action.payload.reviewID)
+        state.likesReviews.push(action.payload.reviewID);
         // 更新课评界面 点赞数
-        for (let i=0; i<state.reviews.length; i++) {
+        for (let i = 0; i < state.reviews.length; i++) {
           if (state.reviews[i].review_id == action.payload.reviewID) {
             state.reviews[i].likes.push(action.payload.openid);
-        }}
+          }
+        }
         // 更新追评界面 点赞数
         if (state.clickedReview.review_id == action.payload.reviewID) {
-          state.clickedReview.likes.push(action.payload.openid)
+          state.clickedReview.likes.push(action.payload.openid);
         }
       })
       .addCase(updateLikes.rejected, () => {
@@ -182,29 +208,30 @@ export const reviewSlice = createSlice({
       })
       // 获取追评
       .addCase(fetchSubReviews.fulfilled, (state, action) => {
-        state.subReviews = action.payload
-        Taro.navigateTo({ url: '/pages/double-review/index'});
+        state.subReviews = action.payload;
+        Taro.navigateTo({ url: "/pages/double-review/index" });
       })
       .addCase(fetchSubReviews.rejected, () => {
-       Taro.showToast({ title: '获取追评时出错了', icon: 'none' })
+        Taro.showToast({ title: "获取追评时出错了", icon: "none" });
       })
       .addCase(fetchSubReviews.pending, () => {
-        Taro.showToast({ title: '搜索中', icon: 'loading'})
+        Taro.showToast({ title: "搜索中", icon: "loading" });
       })
       // 增加追评
       .addCase(addSubReview.fulfilled, (state, action) => {
-        state.subReviews.unshift(action.payload)
+        state.subReviews.unshift(action.payload);
         // review-result页面 评论数+1
-        for (let i=0; i<state.reviews.length; i++) {
+        for (let i = 0; i < state.reviews.length; i++) {
           if (state.reviews[i].review_id == state.clickedReview.review_id) {
             state.reviews[i].numOfComments++;
-        }}
+          }
+        }
         // double-review页面 评论数+1
         state.clickedReview.numOfComments++;
-        Taro.showToast({ title: '添加成功', icon: 'none' })
+        Taro.showToast({ title: "添加成功", icon: "none" });
       })
       .addCase(addSubReview.rejected, () => {
-        Taro.showToast({ title: '添加追评时出错了', icon: 'none' })
+        Taro.showToast({ title: "添加追评时出错了", icon: "none" });
       })
       .addCase(addSubReview.pending, () => {
         // Taro.showToast({ title: '搜索中', icon: 'loading' })
@@ -215,94 +242,119 @@ export const reviewSlice = createSlice({
       })
       .addCase(deleteSubReview.fulfilled, (state, action) => {
         // 删除追评
-        for (let i=0; i<state.subReviews.length; i++) {
+        for (let i = 0; i < state.subReviews.length; i++) {
           if (state.subReviews[i].review_id == action.payload) {
             state.subReviews.splice(i, 1);
-        }}
+          }
+        }
         // review-result页面 评论数-1
-        for (let i=0; i<state.reviews.length; i++) {
+        for (let i = 0; i < state.reviews.length; i++) {
           if (state.reviews[i].review_id == state.clickedReview.review_id) {
             state.reviews[i].numOfComments--;
-        }}
+          }
+        }
         // double-review页面 评论数-1
         state.clickedReview.numOfComments--;
-        Taro.showToast({ title: '删除成功', icon: 'none' })
+        Taro.showToast({ title: "删除成功", icon: "none" });
       })
       .addCase(deleteSubReview.rejected, () => {
-        Taro.showToast({ title: '删除追评时出错了', icon: 'none' })
+        Taro.showToast({ title: "删除追评时出错了", icon: "none" });
       })
       // 修改追评
       .addCase(updateSubReview.pending, () => {
         // Taro.showToast({ title: '修改中', icon: 'loading' })
       })
       .addCase(updateSubReview.fulfilled, (state, action) => {
-        for (let i=0; i<state.subReviews.length; i++) {
+        for (let i = 0; i < state.subReviews.length; i++) {
           if (state.subReviews[i].review_id == action.payload[0]) {
             state.subReviews.splice(i, 1);
-        }}
-        state.subReviews.unshift(action.payload[1])
-        Taro.showToast({ title: '修改成功', icon: 'none' })
+          }
+        }
+        state.subReviews.unshift(action.payload[1]);
+        Taro.showToast({ title: "修改成功", icon: "none" });
       })
       .addCase(updateSubReview.rejected, () => {
-        Taro.showToast({ title: '修改追评时出错了', icon: 'none' })
+        Taro.showToast({ title: "修改追评时出错了", icon: "none" });
       })
       .addCase(fetchUncheckedReviews.pending, () => {
-        Taro.showToast({ title: '拉取中', icon: 'loading' })
-      }) 
+        Taro.showToast({ title: "拉取中", icon: "loading" });
+      })
       .addCase(fetchUncheckedReviews.fulfilled, (state, action) => {
-        state.uncheckedReviews = action.payload
-        console.log("payload :::", action.payload)
-        Taro.showToast({ title: '拉取成功', icon: 'success' })
+        state.uncheckedReviews = action.payload;
+        console.log("payload :::", action.payload);
+        Taro.showToast({ title: "拉取成功", icon: "success" });
       })
       .addCase(markReviewAsPassed.fulfilled, (state, action) => {
-        const courseCode = action.payload[0]
-        const review_id = action.payload[1]
-        const index = state.uncheckedReviews.findIndex(c => c.courseCode == courseCode && c.review_id == review_id)
-        state.uncheckedReviews.splice(index, 1)
-        Taro.showToast({ title: '通过成功', icon: 'success' })
+        const courseCode = action.payload[0];
+        const review_id = action.payload[1];
+        const index = state.uncheckedReviews.findIndex(
+          c => c.courseCode == courseCode && c.review_id == review_id
+        );
+        state.uncheckedReviews.splice(index, 1);
+        Taro.showToast({ title: "通过成功", icon: "success" });
       })
       .addCase(markReviewAsFailed.fulfilled, (state, action) => {
-        const courseCode = action.payload[0]
-        const review_id = action.payload[1]
-        const index = state.uncheckedReviews.findIndex(c => c.courseCode == courseCode && c.review_id == review_id)
-        state.uncheckedReviews.splice(index, 1)
-        Taro.showToast({ title: '不通过成功', icon: 'success' })
+        const courseCode = action.payload[0];
+        const review_id = action.payload[1];
+        const index = state.uncheckedReviews.findIndex(
+          c => c.courseCode == courseCode && c.review_id == review_id
+        );
+        state.uncheckedReviews.splice(index, 1);
+        Taro.showToast({ title: "不通过成功", icon: "success" });
       })
-<<<<<<< HEAD
       .addCase(markSubReviewAsFailed.fulfilled, (state, action) => {
-        const review_id = action.payload
-        const index = state.uncheckedReviews.findIndex(c => c.review_id == review_id)
-        state.allSubReviews.splice(index, 1)
-        Taro.showToast({ title: '不通过成功', icon: 'success' })
+        const review_id = action.payload;
+        const index = state.uncheckedReviews.findIndex(
+          c => c.review_id == review_id
+        );
+        state.allSubReviews.splice(index, 1);
+        Taro.showToast({ title: "不通过成功", icon: "success" });
       })
       .addCase(markSubReviewAsPassed.fulfilled, (state, action) => {
-        const review_id = action.payload
-        const index = state.uncheckedReviews.findIndex(c => c.review_id == review_id)
-        state.allSubReviews.splice(index, 1)
-        Taro.showToast({ title: '通过成功', icon: 'success' })
+        const review_id = action.payload;
+        const index = state.uncheckedReviews.findIndex(
+          c => c.review_id == review_id
+        );
+        state.allSubReviews.splice(index, 1);
+        Taro.showToast({ title: "通过成功", icon: "success" });
       })
-      .addCase(fetchAllSubReviews.fulfilled,(state,action)=>{
+      .addCase(fetchAllSubReviews.fulfilled, (state, action) => {
         state.allSubReviews = action.payload;
-        Taro.showToast({ title: '拉取成功', icon: 'success' })
+        Taro.showToast({ title: "拉取成功", icon: "success" });
       })
       // 获取相关课程代码
       .addCase(searchCourses.pending, () => {
-        Taro.showToast({ title: '搜索中', icon: 'loading', mask: true})
+        Taro.showToast({ title: "搜索中", icon: "loading", mask: true });
       })
       .addCase(searchCourses.fulfilled, (state, action) => {
         state.relevantCourse = action.payload;
-        if( state.relevantCourse.length == 0){
+        if (state.relevantCourse.length == 0) {
           state.searchedCourse = false;
         } else {
           state.searchedCourse = true;
         }
-      })
-=======
->>>>>>> origin/library-unfinished
-}})
+      });
+  }
+});
 
-export const { setSearchedCourse, setClickedReview, setClickedIconReview } = reviewSlice.actions
-export const { setEditModal, setSubReviewEdit, setClickedSubReview } = reviewSlice.actions
-export const { setTurnPage, setReplySubReview, setRelevantCourse } = reviewSlice.actions
-export const { showContentState, setFloatTitle, setModalTitle } = reviewSlice.actions
-export default reviewSlice.reducer
+export const {
+  setSearchedCourse,
+  setClickedReview,
+  setClickedIconReview
+} = reviewSlice.actions;
+export const {
+  setEditModal,
+  setSubReviewEdit,
+  setClickedSubReview
+} = reviewSlice.actions;
+export const {
+  setTurnPage,
+  setReplySubReview,
+  setRelevantCourse
+} = reviewSlice.actions;
+export const {
+  showContentState,
+  setFloatTitle,
+  setModalTitle
+} = reviewSlice.actions;
+export default reviewSlice.reducer;
