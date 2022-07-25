819d81ddb4a97ef08f0babe633a371af0bc4e793
Zian Wang <wza15046319911@github.com>
2022-07-25 12:18:13 +1000

    [merge]准备合并图书馆空位查询功能至main分支;[feat]继续更改manage界面

--- client/src/pages/manage/index.jsx
+++ client/src/pages/manage/index.jsx
@@ -3,7 +3,13 @@ import { useSelector, useDispatch } from "react-redux";
 import Taro, { cloud } from "@tarojs/taro";
 import { View, Text, Button, Image } from "@tarojs/components";
 import ManageCarousel from "../../components/manage/manage-carousel";
-
+import NavBar from "../../components/navbar";
 export default function Manage() {
-  return <ManageCarousel />;
+  return (
+    <View>
+      <NavBar title="管理" backIcon />
+      <ManageCarousel />
+    </View>
+  )
+  // return <ManageCarousel />;
 }
